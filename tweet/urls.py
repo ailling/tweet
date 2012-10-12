51ff0c1dea55c11935efd7c60f2f88458a717a28
Alan Illing <alan.illing@gmail.com>
2012-10-12 15:39:57 -0500

    latest

--- tweet/urls.py
+++ tweet/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', direct_to_template, {'template': 'homepage.html'}),
+    url(r'^live/$', direct_to_template, {'template': 'live.html'}),
     
     # STATIC AND MEDIA CONTENT
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
